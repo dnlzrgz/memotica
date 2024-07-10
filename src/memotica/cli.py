1b216f27604c184d98b0c09aea93577c27ee7cc4
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-07-10 19:33:22 +0200

    feat: add import and export subcommands

--- src/memotica/cli.py
+++ src/memotica/cli.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from memotica.config import Config
 from memotica.db import init_db
+from memotica.models import Deck, Flashcard, Review
+from memotica.repositories import FlashcardRepository, DeckRepository, ReviewRepository
 from memotica.tui import MemoticaApp
 
 
@@ -42,8 +44,19 @@ def run(ctx):
         app.run()
 
 
-@cli.command()
+@cli.group(
+    name="export",
+)
 @click.pass_context
+def export_command_group(_):
+    """
+    Exports your decks, flashcards, and reviews all in one ZIP
+    file, or just your flashcards in a CSV document.
+    """
+    pass
+
+
+@export_command_group.command(name="all")
 @click.option(
     "--path",
     default=".",
@@ -52,27 +65,160 @@ def run(ctx):
         file_okay=False,
         dir_okay=True,
     ),
-    help="Path to the directory where the export file will be saved. Defaults to the current directory.",
+    help="Path to the directory where the export file will be saved.",
+    show_default=True,
 )
-def export(ctx, path):
+@click.pass_context
+def export_all(ctx, path):
     """
-    Exports your memotica data.
-
-    This command exports your memotica data (decks, flashcards, and reviews) into
-    a ZIP file containing a CSV for each table. The ZIP file is saved in the
-    specified directory.
+    Export all your decks, flashcards, and reviews to a ZIP file
+    in the specified directory.
     """
 
     engine = ctx.obj["engine"]
-
     today = datetime.now()
-    zip_filename = os.path.join(path, f"memotica_{today.strftime('%Y-%m-%d')}.zip")
+    zip_filename = os.path.join(path, f'memotica_{today.strftime('%Y-%m-%d')}.zip')
 
     with zipfile.ZipFile(zip_filename, "w") as zipf:
-        for table_name in ["decks", "flashcards", "reviews"]:
-            query = f"SELECT * FROM {table_name}"
-            df = pd.read_sql_query(query, engine)
-            txt = df.to_csv(index=False)
-            zipf.writestr(f"{table_name}.csv", txt)
+        for table in ["decks", "flashcards", "reviews"]:
+            df = pd.read_sql_query(
+                f"SELECT * FROM {table}",
+                engine,
+            )
+            csv_content = df.to_csv(index=False)
+            zipf.writestr(f"{table}.csv", csv_content)
 
-    click.echo("Data exported successfully!")
+        click.echo(f"Data exported successfully to '{zip_filename}'!")
+
+
+@export_command_group.command(name="flashcards")
+@click.option(
+    "--file",
+    "-f",
+    default="flashcards.csv",
+    type=click.Path(
+        exists=False,
+        file_okay=True,
+        dir_okay=False,
+    ),
+    show_default=True,
+    help="Name of the file to export flashcards to.",
+)
+@click.pass_context
+def export_flashcards(ctx, file):
+    """
+    Export your flashcards to a CSV file.
+
+    Note that this export file only includes decks that contains
+    flashcards.
+    """
+    engine = ctx.obj["engine"]
+    df = pd.read_sql_query(
+        """
+        SELECT f.front, f.back, f.reversible, d.name as deck
+        FROM flashcards AS f
+        LEFT JOIN decks AS d ON f.deck_id = d.id
+        """,
+        engine,
+    )
+    df.to_csv(file, index=False)
+
+    click.echo(f"Flashcards exported successfully to {file}")
+
+
+@cli.group(name="import")
+@click.pass_context
+def import_command_group(_):
+    """
+    Import your decks, flashcards, and reviews.
+    """
+    pass
+
+
+@import_command_group.command(name="all")
+@click.argument(
+    "file",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+    ),
+)
+@click.pass_context
+def import_all(ctx, file):
+    """
+    Import all your data (decks, flashcards, and reviews) from a ZIP file.
+    """
+
+    engine = ctx.obj["engine"]
+    import_files = ("decks.csv", "flashcards.csv", "reviews.csv")
+
+    with zipfile.ZipFile(file, "r") as zipf:
+        files_in_zip = zipf.namelist()
+        if len(files_in_zip) != len(import_files):
+            click.echo(
+                f"Somethings is wrong with your backup file. It should contain {len(import_files)} but instead contains {files_in_zip} files!"
+            )
+            return
+
+        if any(file not in files_in_zip for file in import_files):
+            click.echo(
+                "Warning: one or more required files not found in the backup file."
+            )
+            return
+
+        for import_file in import_files:
+            with zipf.open(import_file, "r") as f:
+                df = pd.read_csv(f)
+                df.to_sql(
+                    import_file[:-4],
+                    engine,
+                    if_exists="append",
+                    index=False,
+                )
+
+    click.echo("Data imported successfully!")
+
+
+@import_command_group.command(name="flashcards")
+@click.argument(
+    "file",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+    ),
+)
+@click.pass_context
+def import_flashcards(ctx, file):
+    """
+    Import flashcard from a CSV file.
+
+    This command will import flashcards from a CSV file and create the
+    necessary decks.
+    """
+    engine = ctx.obj["engine"]
+    with Session(engine) as session:
+        flashcards_repository = FlashcardRepository(session)
+        decks_repository = DeckRepository(session)
+        reviews_repository = ReviewRepository(session)
+
+        df = pd.read_csv(file)
+        decks = {}
+        for deck_name in df["deck"].unique():
+            deck = decks_repository.add(Deck(name=deck_name))
+            decks[deck_name] = deck.id
+
+        for _, row in df.iterrows():
+            flashcard = flashcards_repository.add(
+                Flashcard(
+                    front=row["front"],
+                    back=row["back"],
+                    reversible=row["reversible"],
+                    deck_id=decks[row["deck"]],
+                )
+            )
+
+            reviews_repository.add(Review(flashcard=flashcard))
+            if flashcard.reversible:
+                reviews_repository.add(Review(flashcard=flashcard, reversed=True))
